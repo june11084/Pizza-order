95efb193710436dea8ab472628380a021e5f951f
Jun Li <june11084@gmail.com>
2017-12-15 15:16:05 -0800

    have all functions, user can view order detail, shows total price

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 //business logic
 
-var small = 8.00;
-var medium = 10.00;
-var large = 12.00;
+var totalPrice = 0.00;
 
 function Pizza(size, topping) {
   this.size = size;
@@ -10,36 +8,60 @@ function Pizza(size, topping) {
 }
 
 Pizza.prototype.getPizzaPrice = function() {
-   if(this.size === "small"){
-     var price = small;
-   } else if(this.size === "medium"){
-     var price = medium;
-   } else if(this.size === "large"){
-     var price = large;
+   if(this.size === "Small"){
+     var price = 8.00;
+   } else if(this.size === "Medium"){
+     var price = 10.00;
+   } else if(this.size === "Large"){
+     var price = 12.00;
    }
-   return price;
+   if(this.topping.toString().match(/(?=.*Cheese)/)){
+     price += 1;
+   }
+   if(this.topping.toString().match(/(?=.*Pepperoni)/)){
+     price += 1;
+   }
+   if(this.topping.toString().match(/(?=.*Mushrooms)/)){
+     price += 1.5;
+   }
+   if(this.topping.toString().match(/(?=.*Sausage)/)){
+     price += 2;
+   }
+   if(this.topping.toString().match(/(?=.*Bacon)/)){
+     price += 2;
+   }
+   totalPrice += price;
+   return ("$" + price);
 }
 
+Pizza.prototype.printList = function() {
+  return this.size + ", " + this.topping;
+};
 
-
-//UI logic
 
 $(document).ready(function() {
-
   $("form#userInput").submit(function(event) {
     event.preventDefault();
+    var topping = [];
     var size = $("#size").val();
     console.log(size)
-    var topping = $("input:checkbox[name=topping]:checked").each(function(){
-     var topping = $(this).val();
+    $("input:checkbox[name=topping]:checked").each(function(){
+     var toppings = $(this).val();
+     topping.push(toppings);
      console.log(topping)
    });
 
-
     var newOrder = new Pizza(size, topping);
 
+    var price = newOrder.getPizzaPrice();
+    $("ol#orders").append("<li><span class='order'>" + newOrder.printList() + "</span></li>");
+    $("#total").text("$" + totalPrice);
 
-    var result = newOrder.getPizzaPrice();
-    $("#result").append("Your pizza cost: " + result);
+    $(".order").last().click(function() {
+      $("#show-details").show();
+      $(".size").text(newOrder.size);
+      $(".toppings").text(newOrder.topping);
+      $(".cost").text(price);
+    });
   });
 });
